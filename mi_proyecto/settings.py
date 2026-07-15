ce83ec5cb9721185c1de8bc650153946e3a94a0f
kevinN <kevinnishi56@gmail.com>
2026-07-15 16:25:02 -0300

    Fix Render configuration

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = os.getenv("STATIC_URL", "/static/")
-STATIC_ROOT = Path(os.getenv("STATIC_ROOT", str(BASE_DIR / "staticfiles")))
+STATIC_ROOT = Path(os.getenv("STATIC_ROOT", str(BASE_DIR / "staticfile")))
 MEDIA_URL = os.getenv("MEDIA_URL", "/media/")
 MEDIA_ROOT = Path(os.getenv("MEDIA_ROOT", str(BASE_DIR / "media")))
 
